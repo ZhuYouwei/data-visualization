19751c2bb4fc1e19128cd60e987909ecd14da91f
Zhu Youwei <zhu.youwei@gmail.com>
2020-07-17 11:25:56 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "./styles.css";
+import {VisualizationApp} from "./Visualization"
+export default function App() {
+  return (
+    <VisualizationApp />
+  );
+}
